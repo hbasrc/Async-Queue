650ca08f57f804ded8583498294be9e51d03ca10
Toshio Ito <debug.ito@gmail.com>
2013-01-06 17:14:00 +0900

    CPAN Release v0.02

--- lib/Async/Queue.pm
+++ lib/Async/Queue.pm
@@ -111,11 +111,11 @@ Async::Queue - control concurrency of asynchronous tasks
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
